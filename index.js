a792940394fc70c5f188b63c1d6f0f75b89250fe
Bruna-CGS <drabruna.cgs@gmail.com>
2024-11-12 21:35:31 -0300

    projeto ok

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// Classe Heroi
+
 class Heroi {
-    // Construtor para inicializar as propriedades do herói
+    
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
 
-    // Método atacar
+
     atacar() {
         // Variável para armazenar a descrição do ataque
-        let ataqueDescricao;
+        let ataqueSelecionado;
 
         // Estrutura de decisão para definir o tipo de ataque
         switch (this.tipo) {
             case 'mago':
-                ataqueDescricao = 'usou magia';
+                ataqueSelecionado = 'usou magia';
                 break;
             case 'guerreiro':
-                ataqueDescricao = 'usou espada';
+                ataqueSelecionado = 'usou espada';
                 break;
             case 'monge':
-                ataqueDescricao = 'usou artes marciais';
+                ataqueSelecionado= 'usou artes marciais';
                 break;
             case 'ninja':
-                ataqueDescricao = 'usou shuriken';
+                ataqueSelecionado = 'usou shuriken';
                 break;
             default:
-                ataqueDescricao = 'realizou um ataque desconhecido';
+                ataqueSelecionado= 'realizou um ataque desconhecido';
         }
 
         // Exibir a mensagem final do ataque
@@ -35,14 +35,8 @@ class Heroi {
     }
 }
 
-// Criando alguns exemplos de heróis
-const heroi1 = new Heroi('Merlin', 30, 'mago');
-const heroi2 = new Heroi('Arthur', 35, 'guerreiro');
-const heroi3 = new Heroi('Zhang', 28, 'monge');
-const heroi4 = new Heroi('Hattori', 25, 'ninja');
+const heroi1 = new Heroi('Gendalf', 60, 'mago');
+const heroi2 = new Heroi('Thor', 35, 'guerreiro');
 
-// Chamando o método atacar para cada herói
-heroi1.atacar(); // O mago atacou usando magia
-heroi2.atacar(); // O guerreiro atacou usando espada
-heroi3.atacar(); // O monge atacou usando artes marciais
-heroi4.atacar(); // O ninja atacou usando shuriken
+heroi1.atacar(); 
+heroi2.atacar(); 
